52e6b2c6259c94c8f8d81538e7675dade84dd4b9
eb <9453858-eb3n@users.noreply.gitlab.com>
2022-01-11 15:20:37 +0000

    feat: collect thumbsup comments in #horizon-likes

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import Discord from 'discord.js'
+import Discord, { TextChannel } from 'discord.js'
 import dotenv from 'dotenv'
 import { handleCommand } from './handleCommand'
 import { randomChance } from 'tsu'
@@ -13,10 +13,20 @@ client.on('message', (message) => {
   // make bot ignore own messages
   if (message.author === client.user) return
 
-  if (message.content.startsWith(process.env.DISCORD_PREFIX!)) {
+  if (message.content.startsWith(process.env.DISCORD_PREFIX as string)) {
     handleCommand(message)
   } else if (randomChance(500)) {
     message.react('👍')
+
+    if (message.guild?.id === process.env.SERVER_HORIZON) {
+      const THUMBS_UP = client.channels.cache.get(
+        process.env.CHANNEL_HB_THUMBS_UP as string
+      ) as TextChannel
+
+      THUMBS_UP.send(
+        `I liked **${message.author.username}**'s comment:\n> **${message.content}**`
+      )
+    }
   }
 })
 
